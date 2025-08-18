b47a0a90ce1805390cc0275219a6cf72523a131d
nohan-ahmed <102469863+nohan-ahmed@users.noreply.github.com>
2025-08-19 03:47:35 +0600

    feat(notifications): send async order item notifications via Celery
    - Added post_save signal on OrderItem to notify product owners of new orders
    - Sends notifications asynchronously using send_notification_task
    - Includes order ID, product name, quantity, and subtotal in the notification
    - Prepares system for scalable notifications, ready for future real-time integration

--- orders/signals.py
+++ orders/signals.py
@@ -3,6 +3,10 @@ from django.dispatch import receiver
 from .models import Order, OrderItem
 from notifications.tasks import send_notification_task
 
+
+"""
+Sends a notification to the user when an order is created or updated.
+"""
 @receiver(post_save, sender=Order)
 def send_order_notification(sender, instance, created, **kwargs):
     if created:
@@ -15,7 +19,7 @@ def send_order_notification(sender, instance, created, **kwargs):
         
         pass
         
-    else:
+    else:        
         send_notification_task.delay(
             recipient=instance.user,
             title=f'{instance.status} Order',
@@ -25,3 +29,17 @@ def send_order_notification(sender, instance, created, **kwargs):
         
         
         
+"""
+Sends a notification to the product owner when a new order item is created.
+"""    
+@receiver(post_save, sender=OrderItem)
+def send_order_item_notification(sender, instance, created, **kwargs):
+    if created:
+        rechipient_user = instance.order.product_variant.product.user
+        send_notification_task.delay(
+            recipient=rechipient_user,
+            title=f'A new order has been placed. Order ID: {instance.order.pk}',
+            message=f"Product: {instance.product_variant.product.name} - Quantity: {instance.quantity} - Subtotal: {instance.subtotal}",
+            notification_type='order',
+        )
+              
