21b6b6feec03c95c57314e25610cf6c99923b440
nohan-ahmed <102469863+nohan-ahmed@users.noreply.github.com>
2025-08-23 00:44:48 +0600

    chore(admin): enhance reviews and products admin configuration
    - Improved admin display for Review model (readability & management)
    - Updated Product admin with better fields, filters, and search options
    - Refactored admin.py for cleaner structure and maintainability

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,7 +4,7 @@ from .models import Review, ReviewImage
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['user', 'product', 'rating', 'created_at', 'updated_at']
+    list_display = ['id' ,'user', 'product', 'rating', 'created_at', 'updated_at']
     list_filter = ['rating']
     search_fields = ['user__username', 'product__name']
     
